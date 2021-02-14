8f90c8e52b67783489268d5e0cef8fd4e46296da
thebinario <abimaelfrrr@gmail.com>
2021-02-14 12:54:32 -0300

    creating form and sending data to backend
    Signed-off-by: thebinario <abimaelfrrr@gmail.com>

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+//express esta usando engine ejs
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
+    // body parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+// rotas
 app.get("/", function(req, res) {
-    res.send('helloooo');
+    res.render('home');
+})
+app.get("/:perguntar", function(req, res) {
+    res.render('perguntar');
+})
+app.post("/salvapergunta", function(req, res) {
+    let titulo = req.body.titulo;
+    let descricao = req.body.descricao;
+    res.send(`formulario recebido ${titulo} <br> ${descricao} `);
 })
 
 app.listen('8181');
